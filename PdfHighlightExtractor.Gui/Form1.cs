8efee3efcb6c7abab6dcaa3e6dbca363160d2553
nsubotovsky <54336861+nsubotovsky@users.noreply.github.com>
2020-03-31 12:00:07 -0300

    Add project files.

---
+++ PdfHighlightExtractor.Gui/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfHighlightExtractor.Gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string file = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+
+            List<string> allLines = Extractor.GetHighlights(file);
+
+            this.textBox1.Text = string.Join(Environment.NewLine, allLines);
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+    }
+}
---
+++ PdfHighlightExtractor/Extractor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using iTextSharp.text.pdf;
+
+namespace PdfHighlightExtractor
+{
+    public class Extractor
+    {
+        public static List<string> GetHighlights(string pdfPath)
+        {
+            PdfReader pdfReader = new PdfReader(pdfPath);
+
+            List<string> data = new List<string>();
+
+            for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+            {
+                var page = pdfReader.GetPageN(i);
+                PdfArray annotations = page.GetAsArray(PdfName.ANNOTS);
+
+                if (annotations != null)
+                    foreach (PdfObject annotationDict in annotations.ArrayList)
+                    {
+                        PdfDictionary annotation = (PdfDictionary)PdfReader.GetPdfObject(annotationDict);
+                        PdfString contents = annotation.GetAsString(PdfName.CONTENTS);
+                        // now use the String value of contents
+
+                        if (contents != null)
+                        {
+                            data.Add(contents.ToUnicodeString());
+                        }
+                    }
+            }
+
+            return data;
+        }
+    }
+}
